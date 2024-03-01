9e9e6b299bcbe294394090f7aca480adf1454af7
Kirill Kornakov <kirill.kornakov@gmail.com>
2024-03-01 20:16:15 +0300

    init commit

---
+++ src/main/java/com/otus/highload/HighLoadApplication.java
@@ -0,0 +1,12 @@
+package com.otus.highload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighLoadApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HighLoadApplication.class, args);
+  }
+}
---
+++ src/test/java/com/otus/highload/HighLoadApplicationTests.java
@@ -0,0 +1,11 @@
+package com.otus.highload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighLoadApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
